f2c3eec0e913fa402247128688f596d1e3c39a9d
GiacomoLPirrone <giacomollpirrone@gmail.com>
2020-11-15 22:51:11 +1100

    SongList component now can query for all the songs in the database and will display it to the front page

--- client/src/components/SongList.js
+++ client/src/components/SongList.js
@@ -1,11 +1,31 @@
-function SongList() {
-    return (
-      <div>
-        <ul id="song-list">
-            <li>Song name</li>
-        </ul>
-      </div>
-    );
-  }
+import {gql, useQuery} from '@apollo/client';
+
+const getSongsQuery = gql`
+    {
+        songs {
+            name
+            id
+        }
+    }
+`
+function SongList(){
+
+    const {loading, error, data} = useQuery(getSongsQuery);
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Oops! Something went wrong</p>;
+    
+    //console.log(data);
+
+    else {
+        return (
+            <>
+            {data.songs.map((song, id) => (
+                <li key={id}>{song.name}</li>
+            ))}
+            </>
+        )
+    }
+}
   
-  export default SongList;
+export default SongList;
